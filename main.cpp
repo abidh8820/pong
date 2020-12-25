e659954fba40354c753b97dd00b48de1672d125a
Abid Hasan <abidh8820@gmail.com>
2020-12-25 14:02:46 +0600

    some changes

--- main.cpp
+++ main.cpp
@@ -1,24 +1,49 @@
 #include <bits/stdc++.h>
 
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Color.hpp>
 #include <SFML/Window/Keyboard.hpp>
 
 #include "pong.h"
-
 using namespace std;
 
+void score1(std::string p1s) {
+    sf::Text score;
+
+    if (!font.loadFromFile("arial.ttf")) {
+    }
+    score.setFont(font);
+    score.setString(p1s);
+    score.setCharacterSize(30);
+    score.setPosition(W_w * .3, W_h * .2);
+    score.setFillColor(sf::Color::White);
+    window.draw(score);
+}
+void score2(std::string p2s) {
+    sf::Text score;
+
+    if (!font.loadFromFile("arial.ttf")) {
+    }
+    score.setFont(font);
+    score.setString(p2s);
+    score.setPosition(W_w * .7, W_h * .2);
+    score.setCharacterSize(30);
+    score.setFillColor(sf::Color::White);
+    window.draw(score);
+}
 void game_state() {}
 
 int main() {
     window.setPosition(sf::Vector2i(0, 0));
     window.setFramerateLimit(60);
+
     int player1score = 0;
     int player2score = 0;
 
     int p1y = 30, p2y = W_h - 90;
     int bx = W_wh, by = W_hh;
 
-    int ix = 1, iy = 1, bi = 2;
+    int ix = 1, iy = 1, bi = 5;
 
     bool fx = false;
     while (window.isOpen()) {
@@ -32,29 +57,32 @@ int main() {
         while (true) {
         f:
 
-            while (fx) {
+            while (false) {
                 window.clear(sf::Color::Black);
+                score1(to_string(player1score));
+                score2(to_string(player2score));
                 draw(p1y, p2y, bx, by);
-                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Return)){
-                    fx=false;
+
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return)) {
+                    fx = false;
                     break;
                 }
             }
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-                p1y -= 5;
+                p1y -= 10;
                 p1y = max(0, p1y);
             }
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-                p1y += 5;
-                p1y = min(p1y, 370);
+                p1y += 10;
+                p1y = min(p1y, W_h - 85);
             }
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::J)) {
-                p2y -= 6;
+                p2y -= 10;
                 p2y = max(0, p2y);
             }
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::K)) {
-                p2y += 6;
-                p2y = min(p2y, 370);
+                p2y += 10;
+                p2y = min(p2y, W_h - 85);
             }
 
             if (by == 0) iy *= -1;
@@ -71,13 +99,35 @@ int main() {
                 bx = W_wh;
                 by = W_hh;
                 fx = true;
-                goto f;
+            }
+            if (player1score == 2 || player2score == 2) {
+                sf::Text win;
+                if (player1score == 2)
+                    win.setString("player 1 wins");
+                else
+                    win.setString("player 2 wins");
+                if (!font.loadFromFile("arial.ttf")) {
+                }
+                win.setFillColor(sf::Color::White);
+                win.setCharacterSize(40);
+                win.setFont(font);
+                win.setPosition(W_w * .4, W_h * .45);
+                player1score = player2score = 0;
+
+                while (true) {
+                    window.clear(sf::Color::Black);
+                    window.draw(win);
+                    window.display();
+                    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return)) {
+                        goto f;
+                    }
+                }
             }
 
-            if (bx >= 10 && bx <= 21 && by >= p1y && by <= p1y + 50)
+            if (bx >= 10 && bx <= 21 && by >= p1y && by <= p1y + 85)
                 ix = abs(ix);
             else if (bx >= W_w - 25 && bx <= W_w - 10 && by >= p2y &&
-                     by <= p2y + 50)
+                     by <= p2y + 85)
                 ix = -abs(ix);
             bx += ix * bi, by += iy * bi;
 
@@ -85,6 +135,8 @@ int main() {
             by = min(by, W_h - 5);
 
             window.clear(sf::Color::Black);
+            score1(to_string(player1score));
+            score2(to_string(player2score));
             draw(p1y, p2y, bx, by);
         }
     }
--- pong.h
+++ pong.h
@@ -1,23 +1,23 @@
-#define W_w 720
-#define W_h 420
+#define W_w 1366
+#define W_h 768
 
-#define W_wh 360
-#define W_hh 210
+#define W_wh 782
+#define W_hh 384
 #define paddle_speed = 200
 
 sf::RenderWindow window(sf::VideoMode(W_w, W_h), "Pong", sf::Style::Titlebar);
 sf::Font font;
 
 void paddle1_draw(int p1y) {
-    sf::RectangleShape paddle1(sf::Vector2f(10, 50));
+    sf::RectangleShape paddle1(sf::Vector2f(10, 85));
     paddle1.setPosition(10, p1y);
     paddle1.setFillColor(sf::Color::White);
     window.draw(paddle1);
 }
 
 void paddle2_draw(int p2y) {
-    sf::RectangleShape paddle2(sf::Vector2f(10, 50));
-    paddle2.setPosition(700, p2y);
+    sf::RectangleShape paddle2(sf::Vector2f(10, 85));
+    paddle2.setPosition(W_w - 20, p2y);
     paddle2.setFillColor(sf::Color::White);
     window.draw(paddle2);
 }
@@ -32,19 +32,18 @@ void draw(int p1y, int p2y, int bx, int by) {
     paddle1_draw(p1y);
     paddle2_draw(p2y);
     ball_draw(bx, by);
-
     window.display();  // displays the buffer
 }
 
 void init_state() {
-    sf::Text text;                  // initializing text class
+    sf::Text text;  // initializing text class
     if (!font.loadFromFile("arial.ttf")) {
     }
     text.setFont(font);             // setiing the default font
     text.setString("Hello pong!");  // giving the text to print
     text.setCharacterSize(50);
     text.setFillColor(sf::Color::White);
-    text.setPosition(230, 180);
+    text.setPosition(W_w * .4, W_h*.45);
     window.clear(sf::Color::Black);
     window.draw(text);  // draws the string to buffer
     window.display();
